0a7c17b7fa834a612327327a6f1eed882d469b6c
MatiasLMontechiari <matias.monte@davinci.edu.ar>
2021-08-04 19:49:36 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ document.addEventListener("DOMContentLoaded", async function () {
   try {
     const app = document.querySelector(".app")
     let videoCardsTemplate = "",
-      scorebatAPI = `https://www.scorebat.com/video-api/v1/`,
-      scorebatFetch = fetch(scorebatAPI),
-      [scorebatRes] = await Promise.all([scorebatFetch]),
-      scorebatData = await scorebatRes.json();
+        scorebatAPI = `https://www.scorebat.com/video-api/v1/`,
+        scorebatFetch = fetch(scorebatAPI),
+        [scorebatRes] = await Promise.all([scorebatFetch]),
+        scorebatData = await scorebatRes.json();
 
     //Solo traemos las HightLights solo de la Copa America y La Copa del Mundo que ya incluyen los goles y mejores jugadas del partido
     for (let i = 0; i < scorebatData.length; i++) {
@@ -13,6 +13,7 @@ document.addEventListener("DOMContentLoaded", async function () {
         
         let buscarCopaAmerica = scorebatData[i].competition.name.indexOf("COPA AMERICA:")
         let buscarCopaDelMundo = scorebatData[i].competition.name.indexOf("WORLD CUP:")
+        let buscarChampions = scorebatData[i].competition.name.indexOf("CHAMPIONS LEAGUE:")
 
     //Genera las cards con los highlight de la copa america
         if (buscarCopaAmerica !== -1) {
@@ -41,6 +42,21 @@ document.addEventListener("DOMContentLoaded", async function () {
                 </div>`
           }
         }
+      //Genera las cards con los hightlights de la copa del mundo
+      if (buscarChampions !== -1) {
+        if (scorebatData[i].videos[j].title == 'Highlights') {
+          videoCardsTemplate += `
+              <div class="card">
+              ${scorebatData[i].videos[j].embed}
+              <div class="card-body">
+              <h5 class="card-title">${scorebatData[i].title}</h5>
+              <p class="card-text">${scorebatData[i].competition.name}</p>
+              </div>
+              </div>`
+        }
+      }
+
+    //TODO Liga Argentina de Futbol cuando publiquen en el JSON alguno para ver como filtrarlos
       }
     }
     app.innerHTML = videoCardsTemplate;
